950a34483660f8003ad6e4e4532d8199754e3699
Om Gupta <ogupta4242@gmail.com>
2024-10-06 16:56:38 -0700

    [feat] Changed the base page's text [ref] Applied ktfmt google

--- composeApp/src/androidMain/kotlin/omydagreat/github/io/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/omydagreat/github/io/MainActivity.kt
@@ -7,17 +7,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
 
-        setContent {
-            App()
-        }
-    }
+    setContent { App() }
+  }
 }
 
 @Preview
 @Composable
 fun AppAndroidPreview() {
-    App()
+  App()
 }
--- composeApp/src/commonMain/kotlin/omydagreat/github/io/App.kt
+++ composeApp/src/commonMain/kotlin/omydagreat/github/io/App.kt
@@ -10,28 +10,25 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
 import celery.composeapp.generated.resources.Res
 import celery.composeapp.generated.resources.compose_multiplatform
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
+  MaterialTheme {
+    var showContent by remember { mutableStateOf(false) }
+    Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+      Button(onClick = { showContent = !showContent }) { Text("Click me!") }
+      AnimatedVisibility(showContent) {
+        val greeting = remember { Greeting().greet() }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+          Image(painterResource(Res.drawable.compose_multiplatform), null)
+          Text(greeting)
         }
+      }
     }
+  }
 }
