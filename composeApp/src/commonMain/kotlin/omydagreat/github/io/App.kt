8069db6eddf899b94c6c482d292ca29cbb1a8d6a
Malefic <ogupta4242@gmail.com>
2024-10-07 23:12:15 -0700

    [feat] Added multiple screens and navigation [doc] Everything

--- composeApp/src/commonMain/kotlin/omydagreat/github/io/App.kt
+++ composeApp/src/commonMain/kotlin/omydagreat/github/io/App.kt
@@ -1,34 +1,25 @@
 package omydagreat.github.io
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import celery.composeapp.generated.resources.Res
-import celery.composeapp.generated.resources.compose_multiplatform
-import org.jetbrains.compose.resources.painterResource
+import omydagreat.github.io.Common.Screen
+import omydagreat.github.io.Details.DetailsScreen
+import omydagreat.github.io.Home.HomeScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
+/**
+ * Composable function that represents the main application.
+ * It sets up the MaterialTheme and handles navigation between screens.
+ */
 @Composable
 @Preview
 fun App() {
   MaterialTheme {
-    var showContent by remember { mutableStateOf(false) }
-    Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-      Button(onClick = { showContent = !showContent }) { Text("Click me!") }
-      AnimatedVisibility(showContent) {
-        val greeting = remember { Greeting().greet() }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-          Image(painterResource(Res.drawable.compose_multiplatform), null)
-          Text(greeting)
-        }
-      }
+    var currentScreen by remember { mutableStateOf<Screen>(Screen.Home) }
+
+    when (currentScreen) {
+      is Screen.Home -> HomeScreen(onNavigate = { currentScreen = it })
+      is Screen.Details -> DetailsScreen(onBack = { currentScreen = Screen.Home })
     }
   }
 }
---
+++ composeApp/src/commonMain/kotlin/omydagreat/github/io/Details/Greeting.kt
@@ -0,0 +1,19 @@
+package omydagreat.github.io.Details
+
+import omydagreat.github.io.getPlatform
+
+/**
+ * A class that provides a greeting message.
+ */
+class Greeting {
+  private val platform = getPlatform()
+
+  /**
+   * Returns a greeting message that includes the platform name.
+   *
+   * @return A greeting message string.
+   */
+  fun greet(): String {
+    return "( ･ω･)ﾉ ${platform.name}"
+  }
+}
