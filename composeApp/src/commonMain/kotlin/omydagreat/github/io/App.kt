90380010898b25a9f25315a32b83f42894178489
Malefic <ogupta4242@gmail.com>
2024-10-09 12:44:35 -0700

    [feat] Added dark/light mode theming

--- composeApp/src/commonMain/kotlin/omydagreat/github/io/App.kt
+++ composeApp/src/commonMain/kotlin/omydagreat/github/io/App.kt
@@ -1,7 +1,10 @@
 package omydagreat.github.io
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import omydagreat.github.io.Common.Global
 import omydagreat.github.io.Common.Screen
 import omydagreat.github.io.Details.DetailsScreen
@@ -15,10 +18,12 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Composable
 @Preview
 fun App() {
-  MaterialTheme {
-    when (Global.currentScreen) {
-      is Screen.Home -> HomeScreen { screen -> Global.currentScreen = screen }
-      is Screen.Details -> DetailsScreen { Global.currentScreen = Screen.Home }
+  MaterialTheme(colors = Global.theme) {
+    Box(modifier = Modifier.background(Global.theme.background)) {
+      when (Global.currentScreen) {
+        is Screen.Home -> HomeScreen { screen -> Global.currentScreen = screen }
+        is Screen.Details -> DetailsScreen { Global.currentScreen = Screen.Home }
+      }
     }
   }
 }
