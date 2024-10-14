7e41b3e5c29fbfc08530228722239d371437864e
Malefic <ogupta4242@gmail.com>
2024-10-14 13:14:53 -0700

    [ref] Redid CustomText format Same functionality

--- composeApp/src/commonMain/kotlin/omydagreat/github/io/Common/Util/CustomText.kt
+++ composeApp/src/commonMain/kotlin/omydagreat/github/io/Common/Util/CustomText.kt
@@ -1,51 +1,48 @@
+@file:Suppress("unused")
+
 package omydagreat.github.io.Common.Util
 
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import omydagreat.github.io.Common.Global
 
 @Composable
-fun Heading1(text: String) {
-  Text(
-    text = text,
-    style = MaterialTheme.typography.h1,
-    color = Global.theme.onBackground
-  )
-}
+fun Heading1(text: String) = Text(
+  text = text, style = typography.h1, color = Global.theme.onBackground
+)
 
 @Composable
-fun Heading2(text: String) {
-  Text(
-    text = text,
-    style = MaterialTheme.typography.h2,
-    color = Global.theme.onBackground
-  )
-}
+fun Heading2(text: String) = Text(
+  text = text, style = typography.h2, color = Global.theme.onBackground
+)
 
 @Composable
-fun Heading3(text: String) {
-  Text(
-    text = text,
-    style = MaterialTheme.typography.h3,
-    color = Global.theme.onBackground
-  )
-}
+fun Heading3(text: String) = Text(
+  text = text, style = typography.h3, color = Global.theme.onBackground
+)
 
 @Composable
-fun Body1(text: String) {
-  Text(
-    text = text,
-    style = MaterialTheme.typography.body1,
-    color = Global.theme.onBackground
-  )
-}
+fun Heading4(text: String) = Text(
+  text = text, style = typography.h4, color = Global.theme.onBackground
+)
 
 @Composable
-fun Body2(text: String) {
-  Text(
-    text = text,
-    style = MaterialTheme.typography.body2,
-    color = Global.theme.onBackground
-  )
-}
+fun Heading5(text: String) = Text(
+  text = text, style = typography.h5, color = Global.theme.onBackground
+)
+
+@Composable
+fun Heading6(text: String) = Text(
+  text = text, style = typography.h6, color = Global.theme.onBackground
+)
+
+@Composable
+fun Body1(text: String) = Text(
+  text = text, style = typography.body1, color = Global.theme.onBackground
+)
+
+@Composable
+fun Body2(text: String) = Text(
+  text = text, style = typography.body2, color = Global.theme.onBackground
+)
