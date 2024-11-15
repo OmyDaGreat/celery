5b18d859fc083618e22d04879c1e92b7064842a8
Malefic <om@malefic.xyz>
2024-11-14 23:14:49 -0800

    refactor(app): move tbox to outside and set routes as constants

--- composeApp/src/commonMain/kotlin/omydagreat/github/io/App.kt
+++ composeApp/src/commonMain/kotlin/omydagreat/github/io/App.kt
@@ -11,6 +11,10 @@ import omydagreat.github.io.Home.HomeScreen
 import omydagreat.github.io.Stocks.StocksScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
+const val HOME_ROUTE = "/home"
+const val DETAILS_ROUTE = "/details"
+const val STOCKS_ROUTE = "/stocks/{symbol}"
+
 /**
  * Composable function that represents the main application.
  * It sets up the MaterialTheme and handles navigation between screens.
@@ -19,23 +23,19 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 fun App() = PreComposeApp {
   val navigator = rememberNavigator()
-  NavHost(
-    navigator = navigator, initialRoute = "/home"
-  ) {
-    scene(route = "/home") {
-      TBox {
-        HomeScreen(navigator, "/details", "/stocks/MSFT")
+  TBox {
+    NavHost(
+      navigator = navigator, initialRoute = HOME_ROUTE
+    ) {
+      scene(route = HOME_ROUTE) {
+        HomeScreen(navigator, DETAILS_ROUTE, STOCKS_ROUTE.replace("{symbol}", "MSFT"), listOf("AAPL", "GOOGL", "TSLA"))
       }
-    }
-    scene(route = "/details") {
-      TBox {
-        DetailsScreen(navigator, "/home")
+      scene(route = DETAILS_ROUTE) {
+        DetailsScreen(navigator, HOME_ROUTE)
       }
-    }
-    scene(route = "/stocks/{symbol}") { backStackEntry ->
-      val symbol: String? = backStackEntry.path<String>("symbol")
-      TBox {
-        StocksScreen(navigator, "/home", symbol ?: "AAPL")
+      scene(route = STOCKS_ROUTE) { backStackEntry ->
+        val symbol: String? = backStackEntry.path<String>("symbol")
+        StocksScreen(navigator, HOME_ROUTE, symbol ?: "AAPL")
       }
     }
   }
